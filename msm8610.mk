e4fb9e948ebcae71d9fde7252f7dd616376fff0d
Sultan Qasim Khan <sultanqasim@gmail.com>
2017-02-26 22:27:22 -0500

    msm8610-common: clean up audio targets
    audio.msm8610 doesn't exist
    
    Change-Id: I833fa12ce7fe7e32c6948f96acaf7a3cd58919a0

--- msm8610.mk
+++ msm8610.mk
@@ -71,6 +71,19 @@ PRODUCT_AAPT_PREF_CONFIG := hdpi
 DEVICE_PACKAGE_OVERLAYS += \
     $(COMMON_PATH)/overlay
 
+# Audio
+PRODUCT_PACKAGES += \
+    audiod \
+    audio_policy.msm8610 \
+    audio.primary.msm8610 \
+    audio.a2dp.default \
+    audio.usb.default \
+    audio.r_submix.default \
+    libaudio-resampler \
+    tinymix \
+    libqcomvisualizer \
+    libqcomvoiceprocessing
+
 # Audio Configuration
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
@@ -86,10 +99,6 @@ PRODUCT_PACKAGES += \
     hwcomposer.msm8610 \
     memtrack.msm8610 \
     power.msm8610
-
-PRODUCT_PACKAGES += \
-    audio.msm8610 \
-    audio_policy.msm8610
 
 # Browser
 PRODUCT_PACKAGES += \
@@ -107,17 +116,6 @@ PRODUCT_COPY_FILES += \
 
 # Keystore
 PRODUCT_PACKAGES += keystore.msm8610
-
-PRODUCT_PACKAGES += \
-    audiod \
-    audio.primary.msm8610 \
-    audio.a2dp.default \
-    audio.usb.default \
-    audio.r_submix.default \
-    libaudio-resampler \
-    tinymix \
-    libqcomvisualizer \
-    libqcomvoiceprocessing
 
 # Media Config
 PRODUCT_COPY_FILES += \
